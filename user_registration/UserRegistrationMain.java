4fef2c8e551835ea04ce20f7621380696aa805d1
Swapnil_Kamble <swapnilskamble010@gmail.com>
2022-04-08 16:38:48 +0530

    Add: Welcome message added

---
+++ user_registration/UserRegistrationMain.java
@@ -0,0 +1,9 @@
+package regex.user_registration;
+
+public class UserRegistrationMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to User Registration Validation Program");
+	}
+
+}
