8ba2546e3d8910fca35fcf772499375fa55fe8d4
Swapnil_Kamble <swapnilskamble010@gmail.com>
2022-04-16 23:21:21 +0530

    Add: UC1 added for validation of first name entered by user

--- user_registration/UserRegistrationMain.java
+++ user_registration/UserRegistrationMain.java
@@ -1,9 +1,26 @@
 package regex.user_registration;
 
-public class UserRegistrationMain {
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
+public class UserRegistrationMain {
+	Scanner sc = new Scanner(System.in);
+	
+	public void validateFirstName() {
+		System.out.print("Enter first Name : ");
+        String firstName = sc.next();
+        if(firstName.matches("^[A-Z]{1}[A-Za-z]{2,}$")){
+        	System.out.println(firstName + " -\tvalidation success..!");
+        }
+        else {
+        	System.out.println(firstName + " -\tvalidation unsuccessful..!");
+        }
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration Validation Program");
+		
+		UserRegistrationMain userRegistration = new UserRegistrationMain();
+		userRegistration.validateFirstName();
 	}
-
 }
