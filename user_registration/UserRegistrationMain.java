253095a011f2e8b8ae8a6ec8a3219d56aad64057
Swapnil_Kamble <swapnilskamble010@gmail.com>
2022-04-17 00:14:08 +0530

    Add: UC3 added for validation of email ID entered by user

--- user_registration/UserRegistrationMain.java
+++ user_registration/UserRegistrationMain.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class UserRegistrationMain {
 	Scanner sc = new Scanner(System.in);
 
-	public void validateFirstName() {
+	private void validateFirstName() {
 		System.out.print("Enter your First Name : ");
 		String firstName = sc.next();
 		if(firstName.matches("^[A-Z]{1}[A-Za-z]{2,}$")){
@@ -26,12 +26,24 @@ public class UserRegistrationMain {
 			System.out.println(lastName + " -\tvalidation unsuccessful..!");
 		}
 	}
-
+	
+	private void validateEmail() {
+		System.out.print("Enter your Email ID : ");
+		String emailID = sc.next();
+		if(emailID.matches("^[A-Za-z0-9]+[.(A-Za-z0-9)]*@[a-zA-Z]+[.][a-zA-Z]+[.(A-Za-z)]*$")){
+			System.out.println(emailID + " -\tvalidation success..!");
+		}
+		else {
+			System.out.println(emailID + " -\tvalidation unsuccessful..!");
+		}
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration Validation Program");
 
 		UserRegistrationMain userRegistration = new UserRegistrationMain();
 		userRegistration.validateFirstName();
 		userRegistration.validateLastName();
+		userRegistration.validateEmail();
 	}
 }
