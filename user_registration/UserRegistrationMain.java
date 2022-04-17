d54184d36b3a632a2c339b1db71efc7507568ef6
Swapnil_Kamble <swapnilskamble010@gmail.com>
2022-04-17 16:47:08 +0530

    Add: UC9 added for validation of all email samples provided separately

--- user_registration/UserRegistrationMain.java
+++ user_registration/UserRegistrationMain.java
@@ -19,11 +19,11 @@ public class UserRegistrationMain {
 		validation(lastName, lastNamePattern);
 	}
 
-	private void validateEmail() {
-		System.out.print("Enter your Email ID : ");
-		String emailID = sc.next();
-		String emailPattern = "^[A-Za-z0-9]+[.(A-Za-z0-9)]*@[a-zA-Z]+[.][a-zA-Z]+[.(A-Za-z)]*$";
-		validation(emailID, emailPattern);
+	private void validateEmail(String[] emailSample) {
+		for(int i = 0; i < emailSample.length; i++) {
+			String emailPattern = "^[\\w+_-]+(?:\\.[\\w+_-]+)*[@][\\w]{1,}([.]{1}[a-z]{2,}){1,2}$";
+			validation(emailSample[i], emailPattern);
+		}
 	}
 
 	private void validateMobileFormat() {
@@ -42,20 +42,23 @@ public class UserRegistrationMain {
 
 	private void validation(String userInput, String pattern ) {
 		if(userInput.matches(pattern)){
-			System.out.println(userInput + " -\tvalidation success..!");
+			System.out.println("'" + userInput + "'" + " - \tvalidation success..!");
 		}
 		else {
-			System.out.println(userInput + " -\tvalidation unsuccessful..!");
+			System.out.println("'" + userInput + "'" + " - \tvalidation unsuccessful..!");
 		}
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration Validation Program");
+		String[] validEmailSample = {"abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com"};
+		String[] invalidEmailSample = {"abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com", ".abc@abc.com", "abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com", "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au" };
 
 		UserRegistrationMain userRegistration = new UserRegistrationMain();
 		userRegistration.validateFirstName();
 		userRegistration.validateLastName();
-		userRegistration.validateEmail();
+		userRegistration.validateEmail(validEmailSample);
+		userRegistration.validateEmail(invalidEmailSample);
 		userRegistration.validateMobileFormat();
 		userRegistration.validatePassword();
 	}
